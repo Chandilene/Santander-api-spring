5aac8d30de2b89aadf7d530dfafa5030996c72b6
Chandilene <chandileneborges@gmail.com>
2024-07-28 14:32:06 -0300

    Initial commit

---
+++ src/main/java/me/dio/Santander_api_spring/SantanderApiSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApiSpringApplication.class, args);
+	}
+
+}
